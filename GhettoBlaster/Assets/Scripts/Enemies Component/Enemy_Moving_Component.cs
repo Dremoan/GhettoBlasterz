f7ce066f9c6d514dd586c57700e8041acfebe509
Unknown <nersonnils@gmail.com>
2018-04-10 15:31:21 +0200

    Pathfinding, debug shoot

---
+++ GhettoBlaster/Assets/Scripts/Enemies Component/Enemy_Moving_Component.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+[RequireComponent(typeof(Seeker))]
+[RequireComponent(typeof(Rigidbody))]
+public class Enemy_Moving_Component : MonoBehaviour {
+
+    public Transform target;
+    public float updateRate = 2f;
+    public Seeker seeker;
+    public Rigidbody body;
+
+    public Path path;
+    public float speed = 100f;
+    public float nextWaypointDistance = 3f;
+
+    private int currentWayPoint;
+    private bool pathIsEnded = false;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            StartNewPath();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (target == null)
+            return;
+        if (path == null)
+            return;
+        if(currentWayPoint >= path.vectorPath.Count)
+        {
+            if (pathIsEnded)
+                return;
+            Debug.Log("End of path reached");
+            pathIsEnded = true;
+            return;
+        }
+        pathIsEnded = false;
+
+        Vector3 dir = (path.vectorPath[currentWayPoint] - transform.position).normalized;
+        body.velocity = dir * speed * Time.fixedDeltaTime;
+        float dist = Vector3.Distance(transform.position, path.vectorPath[currentWayPoint]);
+        if (dist < nextWaypointDistance)
+        {
+            currentWayPoint++;
+            return;
+        }
+        return;
+    }
+
+    public void StartNewPath()
+    {
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("we got a path, did it have an error?" + p.error);
+        if(!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
+
+    IEnumerator UpdatePath()
+    {
+        if (target == null)
+            yield break;
+
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+        yield return new WaitForSeconds(1 / updateRate);
+        StartCoroutine(UpdatePath());
+    }
+
+    public bool GetPathIsEnded()
+    {
+        return pathIsEnded;
+    }
+
+}
---
+++ GhettoBlaster/Assets/Scripts/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public float shootSpeed;
+	public Rigidbody projectileBody;
+	public GameObject launchPlace;
+	[HideInInspector]public bool dispo = true;
+
+
+	private Vector3 dirToTarget;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Shoot ();
+	}
+
+	void Shoot()
+	{
+		projectileBody.velocity = transform.forward.normalized * shootSpeed * Time.fixedDeltaTime;
+		if(Vector3.Distance(transform.position, launchPlace.transform.position) > 20)
+		{
+			DropManagerComponent.RemoveDrop (this);
+		}
+	}
+}
