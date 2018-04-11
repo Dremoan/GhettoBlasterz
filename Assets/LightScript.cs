a3afd24d3b188e4c7d0c4abd142e7089e0e4e78d
GeoffreyPineau <33876597+GeoffreyPineau@users.noreply.github.com>
2018-04-11 16:41:10 +0200

    Faire que le .ignore fonctionne

---
+++ Assets/LightScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour {
+
+
+	public Material pinkLight;
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		
+	}
+}
