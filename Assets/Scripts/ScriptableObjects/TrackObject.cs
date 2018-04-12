0a2cba9997ff13a4096a466d5e027e645090c241
Unknown <f.noel@rubika-edu.com>
2018-04-12 10:23:51 +0200

    Organisation dossier

---
+++ Assets/Scripts/ScriptableObjects/TrackObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class TrackObject : ScriptableObject 
+{
+	public float bpm;
+	public AudioClip[] ambienceTracks;
+	public AudioClip[] blastTracks = new AudioClip[3];
+
+}
